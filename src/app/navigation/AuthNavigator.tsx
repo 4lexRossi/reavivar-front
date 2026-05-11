71ebbc06a4f5fbd9ba44224686101751549ed6d6
4lexrossi <devalexrossi@gmail.com>
2026-05-11 14:34:08 -0300

    feat: adjust navigation

--- src/app/navigation/AuthNavigator.tsx
+++ src/app/navigation/AuthNavigator.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { IntroScreen } from "@/features/onboarding/screens/IntroScreen";
 import { PretestIntroScreen } from "@/features/pretest/screens/PretestIntroScreen";
-import { WelcomeScreen } from "@/features/auth/screens/WelcomeScreen";
+
 import { SignInScreen } from "@/features/auth/screens/SignInScreen";
 import { SignUpScreen } from "@/features/auth/screens/SignUpScreen";
 import type { AuthStackParamList } from "./types";
@@ -19,7 +19,7 @@ export function AuthNavigator() {
     >
       <Stack.Screen name="Intro" component={IntroScreen} />
       <Stack.Screen name="PreTest" component={PretestIntroScreen} />
-      <Stack.Screen name="Welcome" component={WelcomeScreen} />
+
       <Stack.Screen name="SignIn" component={SignInScreen} />
       <Stack.Screen name="SignUp" component={SignUpScreen} />
     </Stack.Navigator>
